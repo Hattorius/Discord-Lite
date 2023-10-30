770bf825b41219ebc47ddc151526acd38bc214aa
Aaron Jonk <AJ@jonk.name>
2023-10-30 21:29:18 +0900

    Loading dots

---
+++ Discord Lite/Assets/LoadingDots.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Discord_Lite.Assets;
+
+public partial class LoadingDots : UserControl
+{
+    public LoadingDots()
+    {
+        InitializeComponent();
+    }
+}
