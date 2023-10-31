67e320ca00e2c26596fb7e6a7c78e92fee77fd67
Aaron Jonk <AJ@jonk.name>
2023-10-31 13:21:47 +0900

    Loading dots after clicking login

--- Discord Lite/ViewModels/LoginViewModel.cs
+++ Discord Lite/ViewModels/LoginViewModel.cs
@@ -3,13 +3,32 @@ using Avalonia.Interactivity;
 
 namespace Discord_Lite.ViewModels;
 
-public class LoginViewModel
+public class LoginViewModel : INotifyPropertyChanged
 {
-    public MainWindow MainWindow;
+    public MainWindow? MainWindow;
+    private bool _isLoginLoading;
+    public bool IsLoginLoading
+    {
+        get => _isLoginLoading;
+        set
+        {
+            _isLoginLoading = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsLoginLoading)));
+        }
+    }
+
+    private bool _deBounce;
     
     public void OnLoginButtonClick()
     {
-        // TODO: Login logic
+        if (_deBounce) return;
+        _deBounce = true;
+        IsLoginLoading = true;
         
+        // TODO: Login logic
+
+        _deBounce = false;
     }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
 }
